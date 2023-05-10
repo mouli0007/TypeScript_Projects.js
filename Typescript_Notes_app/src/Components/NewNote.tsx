0b159c57c5dbc6c1bfbcab9a7f8b694c6cb30448
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-05-10 12:30:09 +0530

    Add files via upload

---
+++ Typescript_Notes_app/src/Components/NewNote.tsx
@@ -0,0 +1,12 @@
+import NoteForm from "./NoteForm";
+
+const NewNote = () => {
+  return (
+    <div>
+          <h1 className="mb-4">New-Note</h1>
+          <NoteForm/>
+    </div>
+  );
+};
+
+export default NewNote;
